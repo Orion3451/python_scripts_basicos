dfa972027b49186c9a6ed493f3661ce41b93f4a8
aku752 <marioluis7526@gmail.com>
2019-03-30 16:58:15 -0400

    update

--- ejercicios/3-variables.py
+++ ejercicios/3-variables.py
@@ -5,6 +5,7 @@ cadena = "Esto es una cadena"
 boleano = False
 curso_codigo_facilito= "python 2"
 print curso_codigo_facilito
-print "Este es un entero", entero
-print cadena
-print "Esto es un boleano", boleano
+print ("Este es un entero", entero)
+print (cadena)
+print ("Esto es un boleano", boleano)
+print ("ESTo es un boleano")
