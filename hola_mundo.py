1b9040bb47e47886499a6c61b84d937afd05e8a5
aku752 <marioluis7526@gmail.com>
2019-04-01 10:54:56 -0400

    update

--- hola_mundo.py
+++ hola_mundo.py
@@ -1,2 +1,3 @@
 print('Hola que tal mundo python')
-herencia multiple no recomendada, metodos estadticos, para circunstancias especiales, de una constante
+herencia multiple no recomendada,
+metodos estadticos, para circunstancias especiales, de una constante
